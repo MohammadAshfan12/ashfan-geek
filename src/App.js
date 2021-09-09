de74aff5c11256637c649cb4f4896dd251ff990d
MohammadAshfan12 <mohammadashfan48@gmail.com>
2021-09-09 12:54:14 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ToggleSwitch from "./components/ToggleSwitch";
 import { useDarkMode } from "./components/contant/useDarkMode";
 import {
